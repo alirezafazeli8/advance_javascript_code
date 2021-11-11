60d5d8a4210638e6050aaec7536f359c980e234f
alireza <alirezafazeli34@gmail.com>
2021-11-11 14:03:30 +0330

    00/8/20

--- Foundation/app.js
+++ Foundation/app.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 // let array = [];
 
@@ -7,16 +7,78 @@
 // }
 
 //fill array with 60000 elements
-const list = new Array(60000).join("1.1").split(".");
+// const list = new Array(60000).join("1.1").split(".");
 
-function removeItemsFromList() {
-  while (list.length > 0) {
-    if (list) {
-      list.pop();
-    } else {
-      removeItemsFromList();
-    }
-  }
+// function removeItemsFromList() {
+//   while (list.length > 0) {
+//     if (list) {
+//       list.pop();
+//     } else {
+//       removeItemsFromList();
+//     }
+//   }
+// }
+
+// removeItemsFromList();
+
+// var likeFood = "pizza";
+
+// var yourFood = function () {
+//   console.log(likeFood);
+
+//   var likeFood = "Hamburger";
+
+//   console.log(likeFood);
+// };
+
+// debugger;
+// yourFood();
+
+// function sayHello() {
+//   sayHello();
+// }
+
+// sayHello();
+
+// function one() {
+//   var varOne;
+// }
+
+// function two() {
+//   var varOne = false;
+//   one();
+// }
+
+// var varOne = true;
+// two();
+
+// function a() {
+//   let name = "hello";
+//   return this;
+// }
+
+// console.log(a());
+
+// const human = {
+//   name: "alrieza",
+//   run() {
+//     console.log(this);
+//   },
+// };
+
+// human.run();
+
+// function a() {
+//   console.log(this);
+// }
+
+// // console.log();
+// a();
+
+function sayMyName() {
+  console.log(this.name + "!");
 }
 
-removeItemsFromList();
+const name = "alireza";
+
+sayMyName();
